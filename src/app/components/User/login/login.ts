d245e213badc864406fcf17d021a2c95dbf5c610
Ayala4519 <ayala32952@gmail.com>
2026-01-26 22:30:24 +0200

    complete login flow and auth service

---
+++ src/app/components/User/login/login.ts
@@ -0,0 +1,55 @@
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { MessageModule } from 'primeng/message';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { InputMaskModule } from 'primeng/inputmask';
+import { AuthenticationService } from '../../../services/authentication-service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, FloatLabelModule, InputTextModule, ButtonModule, PasswordModule, MessageModule, ToastModule, InputMaskModule],
+  templateUrl: './login.html',
+  providers: [MessageService],
+  styleUrl: './login.scss',
+})
+export class Login {
+  messageService = inject(MessageService);
+  loginForm!: FormGroup;
+  authService = inject(AuthenticationService);
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      Email: new FormControl('', [Validators.required, Validators.email]),
+      Password: new FormControl('', [Validators.required, Validators.minLength(8)],),
+    })
+  };
+  
+  isInvalid(name: string) {
+    const control = this.loginForm.get(name);
+    return control ? control.invalid && (control.touched || control.dirty) : false;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { Email, Password } = this.loginForm.value;
+      this.authService.login(Email, Password).subscribe({
+        next: (response) => {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'User logged in successfully!', life: 3000 });
+          console.log('User logged in:', response);
+          this.loginForm.reset();
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: err.error, life: 3000 });
+          console.error('Login error:', err.error);
+        }
+      });
+    }
+  }
+
+}
--- src/app/services/authentication-service.ts
+++ src/app/services/authentication-service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateUser } from '../models/user.model';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { LoginRequest } from '../models/authentication.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,12 @@ export class AuthenticationService {
   baseUrl: string = 'https://localhost:7282/api/User';
   constructor(private http: HttpClient) {}
   login(email: string, password: string) {
-    return this.http.post<any>(`${this.baseUrl}/login`, { email, password });
+    const loginRequest:LoginRequest={Email:email,Password:password};
+    return this.http.post<any>(`${this.baseUrl}/login`, loginRequest).pipe(
+    tap((response: any) => {
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('user', JSON.stringify(response.user));
+    }))
   }
   register( email: string, password: string,first_name: string, last_name: string, phone?: string):Observable<any> {
     const newUser: CreateUser = { Email: email, Password: password, First_name: first_name, Last_name: last_name, Phone: phone };
