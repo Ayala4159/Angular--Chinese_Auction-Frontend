05cdaad41ad47d6b527437c9bb65f7bd145e0493
Ayala4519 <ayala32952@gmail.com>
2026-02-03 20:48:11 +0200

    Start the working on packages

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { GetAllPackages } from '../../components/Packages/get-all-packages/get-all-packages';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GetAllPackages],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
